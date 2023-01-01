3ebafd7dc1b6f8302db320b61e951518f6448cd5
ciscoliving <fdegarson@gmail.com>
2022-12-31 22:39:18 -0800

    string

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = [:3]
-word_last_2 = [8:]
+word_last_2 = [-2:]
 middle_word = [1:8]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
